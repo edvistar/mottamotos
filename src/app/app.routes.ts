f9790bcee0c562710668c26a8f0e87e793bd04b4
jacc89 <laflacalex9@gmail.com>
2025-03-05 16:51:09 -0500

    routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+
+      {
+        path: 'products',
+        loadChildren: () => import('./Product/features/product.routes')
+      },
+      
+];
