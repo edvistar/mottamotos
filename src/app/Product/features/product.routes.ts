e9af28c7ea2d97bc223f0a69299bd75170a0a0c1
jacc89 <laflacalex9@gmail.com>
2025-03-05 17:13:22 -0500

    routes product

--- src/app/Product/features/product.routes.ts
+++ src/app/Product/features/product.routes.ts
@@ -3,11 +3,12 @@ import { Routes } from '@angular/router';
 export default [
   {
     path: '',
-    loadComponent: () => import('./list-product/list-product.component'),
+    loadComponent: () => import('./list-product/list-product.component')
+      .then(m => m.ListProductComponent), // Ensure correct named export
   },
-
   {
     path: 'product/:id',
-    loadChildren: () => import('./detail-product/detail-product.component'),
+    loadComponent: () => import('./detail-product/detail-product.component')
+      .then(m => m.DetailProductComponent), // Ensure correct named export
   }
-] as unknown as Routes;
+] as Routes;
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,9 @@
 import { Routes } from '@angular/router';
 
-
 export const routes: Routes = [
-
-      {
-        path: 'products',
-        loadChildren: () => import('./Product/features/product.routes')
-      },
-      
+  {
+    path: 'products',
+    loadChildren: () => import('./Product/features/product.routes')
+      .then(m => m.default) // Ensure 'ProductRoutes' is properly exported
+  },
 ];
