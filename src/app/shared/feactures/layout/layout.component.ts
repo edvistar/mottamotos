16b19cb1a7a69b29dafd60ba7505577c815104f1
esvi saho <esvi210@hotmail.com>
2025-03-06 18:39:43 -0500

    llamado al layout despues de iniciar sesionxd

--- src/app/shared/feactures/layout/layout.component.ts
+++ src/app/shared/feactures/layout/layout.component.ts
@@ -1,23 +1,43 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
-import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatSidenav, MatSidenavModule} from '@angular/material/sidenav';
 import {MatListModule} from '@angular/material/list';
 import { Router } from '@angular/router';
 import { StorageService } from '../../data-access/storage.service';
 import { CookieService } from 'ngx-cookie-service';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
   imports: [MatToolbarModule,
-    MatIconModule, MatSidenavModule
-    ,MatListModule
+    MatIconModule, MatSidenavModule,
+    MatListModule, MatButtonModule,
+    RouterModule
   ],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
-export class LayoutComponent {
+export class LayoutComponent implements AfterViewInit {
+  @ViewChild('sidenav') sidenav!: MatSidenav;
+
+  ngAfterViewInit() {
+    setTimeout(() => {
+      console.log('Sidenav:', this.sidenav);
+    }, 1000);
+  }
+
+  toggleSidenav() {
+    console.log('Botón clickeado');
+    console.log('Sidenav:', this.sidenav);
+    if (this.sidenav) {
+      this.sidenav.toggle();
+    } else {
+      console.error('El sidenav no está definido');
+    }
+  }
   username: string = '';
 
   constructor(private router: Router, private _storageService: StorageService,
