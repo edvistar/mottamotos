c94517488ee7cc77d0220a280a2a9aac7963052d
jacc89 <laflacalex9@gmail.com>
2025-03-06 22:14:23 -0500

    rutas hijas

--- src/app/shared/feactures/shared.routes.ts
+++ src/app/shared/feactures/shared.routes.ts
@@ -1,9 +1,37 @@
 import { Routes } from "@angular/router";
+import { LayoutComponent } from "./layout/layout.component";
+
 
 export default [
     {
         path: '',
         loadComponent: ()=> import('./layout/layout.component').then(m=>m.LayoutComponent)
+    },
+    {
+        path: '',
+        component: LayoutComponent, // 👈 Este es el contenedor con el <router-outlet>
+        children: [ // 👈 Rutas hijas que se cargan dentro del layout
+         
+            { 
+                path: 'product', 
+                loadChildren: () => import('../../Product/features/product.routes')
+                  .then(m => m.default) // 👈 Carga las rutas del módulo de productos
+            },
+        
+        
+        
+        
+        
+        
+        
+        ] 
+       
+
     }
 
 ] as Routes;
+
+
+
+
+
